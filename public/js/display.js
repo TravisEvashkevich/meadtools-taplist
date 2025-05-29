199662fe0e5a57b63044f4468723b26839d7b328
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-05-29 17:20:21 -0500

    initial commit

---
+++ public/js/display.js
@@ -0,0 +1,5 @@
+"use strict";
+const body = document.getElementsByTagName("body")[0];
+const test = document.createElement("p");
+test.textContent = "This is a test";
+body.appendChild(test);
---
+++ src/display.ts
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName("body")![0];
+
+const test = document.createElement("p");
+
+test.textContent = "This is a test";
+
+body.appendChild(test);
