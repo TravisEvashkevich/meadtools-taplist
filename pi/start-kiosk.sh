011a445efae1fac43588de34dde4951e97f87ca4
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-06-01 14:28:16 -0500

    made changes

--- pi/start-kiosk.sh
+++ pi/start-kiosk.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Wait for the server to be ready
-sleep 10
+# Start HTTP server in the background
+cd /home/meadtools/meadtools-taplist/public
+sudo python3 -m http.server 80 &
+
+# Wait a few seconds to ensure the server is up
+sleep 5
 
 # Launch Chromium in kiosk mode
-chromium-browser --kiosk --app=http://localhost/ --noerrdialogs --disable-infobars --incognito --disable-session-crashed-bubble --autoplay-policy=no-user-gesture-required --window-position=0,0 --start-fullscreen
+chromium-browser --kiosk --app=http://localhost/ \
+  --noerrdialogs \
+  --disable-infobars \
+  --incognito \
+  --disable-session-crashed-bubble \
+  --autoplay-policy=no-user-gesture-required \
+  --window-position=0,0 \
+  --start-fullscreen
--- pi/start-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Navigate to the project public folder
-cd /home/meadtools/meadtools-taplist/public
-
-# Start a simple HTTP server on port 80
-# You may need to use sudo if port 80 is restricted
-sudo python3 -m http.server 80
