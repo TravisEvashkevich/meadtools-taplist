e0a23a5e4e9643b53fd05fcafe10605f8da4ec20
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-06-21 10:48:13 -0500

    changing it again

--- pi/post-taplist-start.sh
+++ pi/post-taplist-start.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
 
-LOGFILE="/tmp/post-taplist.log"
-{
-  echo "--- $(date): post-taplist-start.sh ---"
-  systemctl daemon-reexec
-  systemctl daemon-reload
-
-  for svc in wlan0-static.service hostapd dnsmasq; do
-    if systemctl list-units --full -all | grep -Fq "$svc"; then
-      echo "Restarting $svc..."
-      systemctl restart "$svc"
-    else
-      echo "⚠️  $svc not found"
-    fi
-  done
-} >> "$LOGFILE" 2>&1
+sudo systemctl daemon-reload
+sudo systemctl restart wlan0-static.service
+sudo systemctl restart hostapd
+sudo systemctl restart dnsmasq
